dbfefa976180fcafe381fc9deee9743894bde8c2
Mehtap Isik <Mehtap.Isik@modernatx.com>
2024-08-06 11:52:20 -0400

    Update run_parsefep_du_plot.sh

--- fe_calcs_with_0_or_2_mg/automation_scripts/run_parsefep_du_plot.sh
+++ fe_calcs_with_0_or_2_mg/automation_scripts/run_parsefep_du_plot.sh
@@ -20,7 +20,8 @@ cond_list="9-5NaCl_bb_colvar/3-40winCmplx_2ns"
 rep_list="1-rep1 2-rep2 3-rep3"
 WD=$PWD
 ## set common directory path
-cmn_dir="/home/arasouli/repos/alchemical/rna_small_molecule_FE/common_files"
+#cmn_dir="/home/arasouli/repos/alchemical/rna_small_molecule_FE/common_files"
+cmn_dir="/home/misik/repos/rna_theophylline_free_energy_calculations/fe_calcs_with_0_or_2_mg/common_files"
 
 ## loop over the compounds, conditions and replicas
 for val1 in $dir_list; do
