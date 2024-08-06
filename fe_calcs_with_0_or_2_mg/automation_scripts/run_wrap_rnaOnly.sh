6ae4bc14c3602fdd8ecda3b28362a0a027be4405
Mehtap Isik <Mehtap.Isik@modernatx.com>
2024-08-06 11:57:17 -0400

    Update run_wrap_rnaOnly.sh

--- fe_calcs_with_0_or_2_mg/automation_scripts/run_wrap_rnaOnly.sh
+++ fe_calcs_with_0_or_2_mg/automation_scripts/run_wrap_rnaOnly.sh
@@ -16,7 +16,8 @@ dir_list="8-rna_RMSD_colvar_contr"
 cond_list="1-55NaCl_2Mg"
 rep_list="1-rep1 2-rep2 3-rep3"
 ## set common directory path
-cmn_dir="/home/arasouli/repos/alchemical/3_mg_rna_small_molecule_FE/common_files"
+#cmn_dir="/home/arasouli/repos/alchemical/3_mg_rna_small_molecule_FE/common_files"
+cmn_dir="/home/misik/repos/rna_theophylline_free_energy_calculations/fe_calcs_with_0_or_2_mg/common_files"
 
 ## loop over the compounds, conditions and replicas and wrap
 ## vmd needs to be installed and its path needs to be added to $PATH
