8a2013963a8b32306268ad63469ff50199657b54
Mehtap Isik <Mehtap.Isik@modernatx.com>
2024-08-06 11:45:16 -0400

    Update run_fix_ligOnly1.sh

--- fe_calcs_with_0_or_2_mg/automation_scripts/run_fix_ligOnly1.sh
+++ fe_calcs_with_0_or_2_mg/automation_scripts/run_fix_ligOnly1.sh
@@ -24,7 +24,8 @@ cond_list="9-5NaCl_bb_colvar"
 rep_list=" 1-rep1 3-rep3"
 WD=$PWD
 ## set common directory path
-cmn_dir="/home/arasouli/repos/alchemical/rna_small_molecule_FE/common_files"
+# cmn_dir="/home/arasouli/repos/alchemical/rna_small_molecule_FE/common_files"
+cmn_dir="/home/misik/repos/rna_theophylline_free_energy_calculations/fe_calcs_with_0_or_2_mg/common_files"
 
 ## loop over the compounds, conditions and replicas
 ## and fix the reionize
