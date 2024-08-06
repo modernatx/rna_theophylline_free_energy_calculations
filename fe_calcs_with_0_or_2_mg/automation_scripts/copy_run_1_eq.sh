99f538742311f1ad1fff7e1af1ca46d4f20f3fb8
Mehtap Isik <Mehtap.Isik@modernatx.com>
2024-08-06 11:32:15 -0400

    Update copy_run_1_eq.sh

--- fe_calcs_with_0_or_2_mg/automation_scripts/copy_run_1_eq.sh
+++ fe_calcs_with_0_or_2_mg/automation_scripts/copy_run_1_eq.sh
@@ -9,7 +9,8 @@ WD=$PWD
 
 ## set common directory path
 #cmn_dir="/home/arasouli/repos/alchemical/3_mg_rna_small_molecule_FE/common_files"
-cmn_dir="/home/misik/repos/alchemical_fe_theophylline/common_files"
+#cmn_dir="/home/misik/repos/alchemical_fe_theophylline/common_files"
+cmn_dir="/home/misik/repos/rna_theophylline_free_energy_calculations/fe_calcs_with_0_or_2_mg/common_files"
 
 cd $WD
 for val1 in $dir_list; do
