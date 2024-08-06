fb2fc1d13a69d924d12f21555bd0e792a48dca97
Mehtap Isik <Mehtap.Isik@modernatx.com>
2024-08-06 12:10:26 -0400

    Update copy_run_1_BFEE.sh

--- fe_calcs_with_3_mg/automation_scripts/copy_run_1_BFEE.sh
+++ fe_calcs_with_3_mg/automation_scripts/copy_run_1_BFEE.sh
@@ -9,7 +9,8 @@ WD=$PWD
 
 ## set common directory path
 #cmn_dir="/home/arasouli/repos/alchemical/3_mg_rna_small_molecule_FE/common_files"
-cmn_dir="/home/misik/repos/3_mg_rna_small_molecule_fe/common_files"
+#cmn_dir="/home/misik/repos/3_mg_rna_small_molecule_fe/common_files"
+cmn_dir="/home/misik/repos/rna_theophylline_free_energy_calculations/fe_calcs_with_3_mg/common_files"
 
 ## make sure bfee environment is properly setup before running this script
 # conda activate bfee
