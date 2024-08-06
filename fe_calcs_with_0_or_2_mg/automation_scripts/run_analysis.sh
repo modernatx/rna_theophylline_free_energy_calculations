2e63af22882f6461f59b61ffacd79103b969752c
Mehtap Isik <Mehtap.Isik@modernatx.com>
2024-08-06 11:34:41 -0400

    Update run_analysis.sh

--- fe_calcs_with_0_or_2_mg/automation_scripts/run_analysis.sh
+++ fe_calcs_with_0_or_2_mg/automation_scripts/run_analysis.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #####################################################################
-## This script makes directoris and runs RMSD analysis.            ##
+## This script makes directories and runs RMSD analysis.            ##
 #####################################################################
 
 
@@ -13,7 +13,8 @@ win_list="1-40winCmplx_30winLig "
 rep_list="1-rep1 2-rep2 3-rep3"
 WD=$PWD
 ## set common_files directory path
-cmn_dir="/home/arasouli/repos/alchemical/rna_small_molecule_FE/common_files"
+#cmn_dir="/home/arasouli/repos/alchemical/rna_small_molecule_FE/common_files"
+cmn_dir="/home/misik/repos/rna_theophylline_free_energy_calculations/fe_calcs_with_0_or_2_mg/common_files"
 
 ## loop over the compounds, conditions and replicas
 ## and setup the directories and run rmsd.tcl
