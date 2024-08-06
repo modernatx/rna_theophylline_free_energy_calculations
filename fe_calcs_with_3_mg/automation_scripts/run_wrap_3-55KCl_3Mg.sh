39d4763ea587390ac68a3740ff3c674811b4c9de
Mehtap Isik <Mehtap.Isik@modernatx.com>
2024-08-06 12:35:55 -0400

    Update run_wrap_3-55KCl_3Mg.sh

--- fe_calcs_with_3_mg/automation_scripts/run_wrap_3-55KCl_3Mg.sh
+++ fe_calcs_with_3_mg/automation_scripts/run_wrap_3-55KCl_3Mg.sh
@@ -5,7 +5,7 @@
 ## and saves a pdb and psf in "ini" directory.                     ##
 ## WARNING: make sure to open                                      ##
 ## the saved files and check if the ligand has been properly       ##
-## wrapped. If ligand is streched across the periodic boundry, the ##
+## wrapped. If ligand is stretched across the periodic boundary, the ##
 ## calculations will be faulty.                                    ##
 #####################################################################
 
@@ -14,9 +14,11 @@
 dir_list="3-1_methylxanthine" # "2-theophylline 4-3_methylxanthine 5-hypoxanthine 6-xanthine 7-caffeine"
 cond_list="3-55KCl_3Mg" #"1-55NaCl_3Mg 2-Neut_3Mg"
 rep_list="1-rep1" #"2-rep2 3-rep3"
+
 ## set common directory path
 #cmn_dir="/home/arasouli/repos/alchemical/3_mg_rna_small_molecule_FE/common_files"
-cmn_dir="/home/misik/repos/3_mg_rna_small_molecule_fe/common_files"
+#cmn_dir="/home/misik/repos/3_mg_rna_small_molecule_fe/common_files"
+cmn_dir="/home/misik/repos/rna_theophylline_free_energy_calculations/fe_calcs_with_3_mg/common_files"
 
 ## loop over the compounds, conditions and replicas and wrap
 ## vmd needs to be installed and its path needs to be added to $PATH
