6b58836c521f0f04158471658598b8804a667517
Mehtap Isik <Mehtap.Isik@modernatx.com>
2024-08-06 12:12:03 -0400

    Update run_bb_colvar.sh

--- fe_calcs_with_3_mg/automation_scripts/run_bb_colvar.sh
+++ fe_calcs_with_3_mg/automation_scripts/run_bb_colvar.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #####################################################################
-## Sets up simulation and creates the direcotires and files for backbone 
+## Sets up simulation and creates the directories and files for backbone 
 ## restrained RNA using the COLVAR module of NAMD.
 ## 
 #####################################################################
@@ -11,7 +11,10 @@
 dir_list="2-theophylline"
 cond_list="4-55NaCl_3Mg_bb_colvar"
 rep_list="1-rep1 2-rep2 3-rep3"
-cmn_dir="/home/arasouli/repos/alchemical/3_mg_rna_small_molecule_FE/common_files"
+
+#cmn_dir="/home/arasouli/repos/alchemical/3_mg_rna_small_molecule_FE/common_files"
+cmn_dir="/home/misik/repos/rna_theophylline_free_energy_calculations/fe_calcs_with_3_mg/common_files"
+
 WD=$PWD
 
 mkdir -p {2-theophylline,3-1_methylxanthine,4-3_methylxanthine,5-hypoxanthine,6-xanthine,7-caffeine}/4-55NaCl_3Mg_bb_colvar/1-40winCmplx_30winLig/{1-rep1,2-rep2,3-rep3}
