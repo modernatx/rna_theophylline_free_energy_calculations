2bcac798eedbfb9f57f0f19c7e23e42500146ff7
Mehtap Isik <Mehtap.Isik@modernatx.com>
2024-08-06 12:34:35 -0400

    Update run_make_sys_3-55KCl_3Mg_lig6_rep5.sh

--- fe_calcs_with_3_mg/automation_scripts/run_make_sys_3-55KCl_3Mg_lig6_rep5.sh
+++ fe_calcs_with_3_mg/automation_scripts/run_make_sys_3-55KCl_3Mg_lig6_rep5.sh
@@ -12,8 +12,11 @@ dir_list1="6-xanthine" #" 3-1_methylxanthine 4-3_methylxanthine 5-hypoxanthine 6
 #dir_list2=" 2-theophylline "
 cond_list="3-55KCl_3Mg"
 rep_list="5-rep5" #"1-rep1 2-rep2 3-rep3 4-rep4"
+
 #cmn_dir="/home/arasouli/repos/alchemical/rna_small_molecule_FE/common_files"
-cmn_dir="/home/misik/repos/3_mg_rna_small_molecule_fe/common_files"
+#cmn_dir="/home/misik/repos/3_mg_rna_small_molecule_fe/common_files"
+cmn_dir="/home/misik/repos/rna_theophylline_free_energy_calculations/fe_calcs_with_3_mg/common_files"
+
 WD=$PWD
 
 
